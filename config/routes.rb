75d6a5fd0d2751f6f66442300ed7eb464d59a24a
Tony Maktima <tmaktima@deem.com>
2014-02-05 15:24:19 -0700

    initial code

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 TestProjectDeployTonym::Application.routes.draw do
+  # get "welcome/index"
+    get "hello/index"
+    root "hello#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
