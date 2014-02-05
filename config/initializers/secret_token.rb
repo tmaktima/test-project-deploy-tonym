f7033718eb174ab350cab62e0ed5834c964ee4b3
Tony Maktima <tmaktima@deem.com>
2014-02-05 14:49:31 -0700

    base setup for test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestProjectDeployTonym::Application.config.secret_key_base = '1a4ac06c5d0f72348ee7409e3cb92ed128034bcfbccae60b6b1e9610ec96c072039697735160dd8f446147fe8ad14b89f83e7da3041b3e34d52450b95df0f176'
