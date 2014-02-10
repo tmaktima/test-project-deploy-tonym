c7a9dc4cbc294377e140f3810047add5a8db53c8
Tony Maktima <tmaktima@deem.com>
2014-02-10 14:30:54 -0700

    updated vagrant and welcome

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 TestProjectDeployTonym::Application.routes.draw do
-  # get "welcome/index"
-    get "hello/index"
-    root "hello#index"
+  get "welcome/index"
+    get "welcome/index"
+  # get "hello/index"
+    root "welcome#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
