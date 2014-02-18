fda198d7ad26a63d592790ceec8a9dc032c5e509
Tony Maktima <tmaktima@deem.com>
2014-02-18 13:54:28 -0700

    reverse proxy

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,7 +3,7 @@ pid "vagrant/tmp/pids/unicorn.pids"
 stderr_path "/vagrant/log/unicorn.log"
 stdout_path "/vagrant/log/unicorn.log"
 
-listen "/tmp/unicorn.todo.sock"
+listen "/tmp/unicorn.shopper.sock"
 worker_processes 2
 timeout 30
-/tmp/unicorn.todo.sock
+/tmp/unicorn.shopper.sock
